6774476ba9b4596f2cd6ec008191465a11ad900e
aonyshchuk <aonyshchuk@apple.com>
2020-04-29 14:18:11 -0700

    add reflection based method to see what extra options are now set to Reader/Writer

--- src/main/scala/com/github/andyglow/spark/options/ApplyOptionAdapter.scala
+++ src/main/scala/com/github/andyglow/spark/options/ApplyOptionAdapter.scala
@@ -1,5 +1,8 @@
 package com.github.andyglow.spark.options
 
+import scala.collection.mutable
+import scala.reflect.ClassTag
+
 
 class ApplyOptionAdapter[SUBJ](r: SUBJ) {
 
@@ -9,4 +12,12 @@ class ApplyOptionAdapter[SUBJ](r: SUBJ) {
 
   def options[E[_], T](kv: Map[String, E[T]])(implicit apply: ApplySparkOption[SUBJ, (String, E[T])]): SUBJ =
     kv.foldLeft(r) { case (r, (k, ev)) => r.option(k, ev) }
+
+  def optionMap(implicit clazz: ClassTag[SUBJ], ev: ApplySparkOption[SUBJ, (String, String)]): Map[String, String] = {
+    val f = clazz.runtimeClass.getDeclaredField("extraOptions")
+    f.setAccessible(true)
+
+    val extraOptions = f.get(r).asInstanceOf[mutable.Map[String, String]]
+    extraOptions.toMap
+  }
 }
